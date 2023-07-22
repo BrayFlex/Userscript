a73e5635c3509896e4af3aec307297e1f9e9b753
Benjamin Barbeau <benjamin@barbeau.net>
2023-07-21 21:37:18 -0400

    Add port

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-export const CHIEF_WS_ENDPOINT = ___WS_ENDPOINT___ || 'wss://place.xeondigital.ca/ws';
+export const CHIEF_WS_ENDPOINT = ___WS_ENDPOINT___ || 'wss://place.xeondigital.ca:3000/ws';
 export const USERSCRIPT_REVISION = ___GIT_HASH___ || 'development';
